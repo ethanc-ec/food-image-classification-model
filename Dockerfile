76a092c3aa67c3ad58111f3e13d5cb33da92682b
Ethan Chang <44012676+ethanc-ec@users.noreply.github.com>
2024-11-12 17:05:04 -0500

    work

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM pytorch/pytorch:2.5.1-cuda12.4-cudnn9-runtime
+
+# Install additional packages
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+    && apt-get -y install --no-install-recommends ffmpeg libsm6 libxext6 \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install Python packages
+COPY pyproject.toml .
+RUN pip install uv && \
+    uv sync --link-mode=copy
--- src/data-script.py
+++ src/data-script.py
@@ -22,26 +22,26 @@ if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    
+
     logger.info(f"Using DATA_PATH: {DATA_PATH}")
-    
+
     if not Path(DATA_PATH / "food101-train.parquet").exists():
         logger.info("Downloading: food101-train dataset")
-        
+
         splits = {'train': 'data/train-*.parquet', 'validation': 'data/validation-*.parquet'}
         pl.read_parquet('hf://datasets/ethz/food101/' + splits['train']).write_parquet(DATA_PATH / "food101-train.parquet")
-        
+
         logger.info("Completed: food101-validation dataset")
 
     else:
         logger.info("food101-train dataset already exists")
-        
+
     if not Path(DATA_PATH / "food101-validation.parquet").exists():
         logger.info("Downloading: food101-validation dataset")
-        
+
         splits = {'train': 'data/train-*.parquet', 'validation': 'data/validation-*.parquet'}
         pl.read_parquet('hf://datasets/ethz/food101/' + splits['validation']).write_parquet(DATA_PATH / "food101-validation.parquet")
-        
+
         logger.info("Completed: food101-validation dataset")
 
     else:
@@ -49,24 +49,24 @@ if __name__ == "__main__":
 
     if not Path(DATA_PATH / "food102-train.parquet").exists():
         logger.info("Downloading: food102-train dataset")
-    
+
         splits = {'train': 'data/train-*-of-*.parquet', 'test': 'data/test-*-of-*.parquet'}
         pl.read_parquet('hf://datasets/juliensimon/food102/' + splits['train']).write_parquet(DATA_PATH / "food102-train.parquet")
-        
+
         logger.info("Completed: food102-train dataset")
-    
+
     else:
         logger.info("food102-train dataset already exists")
-        
+
     if not Path(DATA_PATH / "food102-test.parquet").exists():
         logger.info("Downloading: food102-test dataset")
 
         splits = {'train': 'data/train-*-of-*.parquet', 'test': 'data/test-*-of-*.parquet'}
         pl.read_parquet('hf://datasets/juliensimon/food102/' + splits['test']).write_parquet(DATA_PATH / "food102-test.parquet")
-        
+
         logger.info("Completed: food102-test dataset")
-    
+
     else:
         logger.info("food102-test dataset already exists")
-        
+
     logger.info("Completed: All downloads/checks")
